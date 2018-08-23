558b5d2a8a60994e98339c47d16acde4e7a594d0
Phuong Pham <phuongpham@Phuongs-MacBook-Pro.local>
2018-08-23 15:14:46 +0700

    initial commit

--- web02/Homework/pilotapp_Customer.py
+++ web02/Homework/pilotapp_Customer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import *
 import mlab
 from mongoengine import *
 from models.Customer import Customer 
@@ -24,6 +24,41 @@ def index():
 def customer(g):
     all_customer = Customer.objects[:10](gender = g, contacted = False)
     return render_template('showcustomer.html', all_customer = all_customer)
+
+@app.route('/admin')
+def admin():
+    all_customer = Customer.objects()
+    return render_template('admin.html', all_customer = all_customer)
+
+@app.route('/delete/<customer_id>')
+def delete(customer_id):
+    customer_to_delete = Customer.objects.with_id(customer_id)
+    if customer_to_delete is not None:
+        customer_to_delete.delete()
+        return redirect(url_for('admin'))
+    else:
+        return "Service not found"
+    #return "Deleted " + customer_id
+
+@app.route('/new-customer', methods = ["GET", "POST"])
+def create():
+    if request.method == "GET":
+        return render_template('new-customer.html')
+    elif request.method == "POST":
+        form = request.form
+        name = form['name']
+        yob = form['yob']
+        phone = form['phone']
+
+        new_customer = Customer(
+            name = name,
+            yob = yob,
+            phone = phone
+        )
+#update: set before input value, radio button 
+        new_customer.save()
+        return redirect(url_for('admin'))
+        #return name + yob + phone
 
 if __name__ == '__main__':
   app.run(debug=True)
--- web02/Homework/query_customer.py
+++ web02/Homework/query_customer.py
@@ -3,6 +3,28 @@ import mlab
 
 mlab.connect()
 
+# id_to_find = "5b7be22918fca375eafae0d9"
+
+# #amanda = Customer.objects(id=id_to_find) ## => []
+
+# #amanda = Customer.objects.get(id=id_to_find) ## => Service object
+
+# customer = Customer.objects.with_id(id_to_find) ## => Service object
+
+# if customer is not None:
+#     #print(amanda.name)
+#     #customer.delete()
+#     #print("Deleted")
+#     print("Before: ")
+#     print(customer.to_mongo()) #print(customer.yob)
+#     customer.update(set__yob=2005, set__name="Linh cute")
+#     customer.reload()
+#     print("After: ")
+#     print(customer.to_mongo())
+
+# else:
+#     print("Not found")
+
 all_customer = Customer.objects()
 
 first_customer = all_customer[0]
